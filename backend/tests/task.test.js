9d582153e9acb7576ab61f2bcc5d56729b4b8674
Santhosh <santhoshprogrammer07@gmail.com>
2025-09-05 12:20:48 +0530

    Initial commit

---
+++ backend/tests/task.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const { server, app } = require('../index');
+const { default: mongoose } = require('mongoose');
+
+describe('GET api/tasks', () => {
+    it('it should return 200 ok', async () => {
+        const res = await request(app).get('/api/tasks');
+        expect(res.statusCode).toBe(200);
+    });
+
+    it('it should return an object and tasks property ok', async () => {
+        const res = await request(app).get('/api/tasks');
+        expect(typeof res.body).toBe("object");
+        expect(res.body).toHaveProperty("tasks");
+    })
+})
+
+afterAll(async () => {
+    await server.close();
+    await mongoose.connection.close();
+})
